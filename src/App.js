dd22a4cf86c363cb03976c0259c834464150ff1c
Pyunghyuk Yoo <yoophi@gmail.com>
2019-01-08 15:37:04 +0900

    CRA + react-app-rewired + MobX

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { observable } from "mobx";
+import { observer } from "mobx-react";
+import "./App.css";
+
+class CounterContainer {
+  @observable count = 0;
+}
+
+const store = new CounterContainer();
+
+@observer
+class Counter extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          <code>{store.count}</code>
+        </h1>
+        <button onClick={() => store.count--}>-</button>
+        <button onClick={() => store.count++}>+</button>
+      </div>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Counter />
         </header>
       </div>
     );
